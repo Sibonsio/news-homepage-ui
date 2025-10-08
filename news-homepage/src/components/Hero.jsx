43052734e95b05696f16328e5f284249125a18b3
Sibonsio <siboniso1sekute@gmail.com>
2025-10-08 14:38:51 +0200

    completed the grid for big screens

--- news-homepage/src/components/Hero.jsx
+++ news-homepage/src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = ()=>{
     return(
         <div className='hero'>
             <img className='hero-image' src={heroImage}/>
-            <div className='text-container'><h1 className='title'>The Bright Future of Web 3.0?</h1>
+            <div className='text-container'><div className='title-div'><h1 className='title'>The Bright Future of Web 3.0?</h1></div>
             <div className='button-div'>
             <p className='description'>We dive into the next evolution of the web that claims to put the power 
                 of the platforms back into the hands of the people. But is it really fulfilling its promise?
